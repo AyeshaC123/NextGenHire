0d49b8c3e3a4310e56408c214057a9a8295624d1
Shannon Joseph <shannonjoseph@Shannons-MacBook-Pro-2.local>
2025-02-22 20:43:29 -0500

    auth0 implemented

--- gemini_program/main.py
+++ gemini_program/main.py
@@ -1,8 +1,12 @@
 import google.generativeai as genai
 from flask import Flask, request, render_template
+from dotenv import load_dotenv
+import os
+from flask import session
 
 app = Flask(__name__)
 
+load_dotenv()
 # Replace with your actual Gemini API key
 GOOGLE_API_KEY = "AIzaSyCvFJIH-5t0Uj9QKiXMjzUf4_H6Y3cIZqQ"
 
@@ -20,13 +24,21 @@ def generate_text(prompt):
     except Exception as e:
         return f"An error occurred: {e}"
 
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         prompt = request.form['prompt']
+#         generated_text = generate_text(prompt)
+#         return render_template('index.html', generated_text=generated_text)
+#     return render_template('index.html')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         prompt = request.form['prompt']
         generated_text = generate_text(prompt)
-        return render_template('index.html', generated_text=generated_text)
-    return render_template('index.html')
+        return render_template('index.html', generated_text=generated_text, session=session.get('user'))
+    return render_template('index.html', session=session.get('user'))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
